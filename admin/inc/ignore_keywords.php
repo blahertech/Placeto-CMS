b151f49a21415f8910700d399fe79b5886aafb6a
blaher <byoung@blahertech.com>
2009-11-26 13:47:46 +0000

    admin and setup cleanup
    git-svn-id: https://placeto.svn.sourceforge.net/svnroot/placeto/trunk@103 10ae5500-964b-49ae-bf62-554531ab5dc9

--- admin/inc/ignore_keywords.php
+++ admin/inc/ignore_keywords.php
@@ -1,15 +1,18 @@
 <?php
 	/**
 	*	Placeto CMS - Admin
+	*		The Placeto CMS Administration application.
 	*
-	*	Authors: Benjamin Jay Young, Michael Weidenhamer and Taylor Wyant
-	*		http://www.blahertech.org/projects/placeto
+	*	Copyright (C) 2009 BlaherTech
 	*
-	*	Placeto CMS - Admin (C) BlaherTech - Benjamin Jay Young 2009
-    *	Placeto CMS Admin is released under the GNU GPL 3.0 and is free and open source.
-	*	You may edit or distrubute Placeto CMS Admin at your own free will, with the proper accreditation.
+	*	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+	*	This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+	*	You should have received a copy of the GNU General Public License along with this program, as license.txt.  If not, see <http://www.gnu.org/licenses/>.
 	*
-	*	////////////////////////////////////////
+	*	Author: Benjamin Jay Young
+	*		http://www.blahertech.org/projects/placeto/
+	*
+	*	//////////////////////////////////////////////////
 	*
 	*	ignore_keywords.php is a HUGE list of common words that are not proper SEO keywords.
 	**/

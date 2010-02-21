6bc717497630cb7b38f80a47a3e90ea584420c29
blaher <byoung@blahertech.com>
2010-02-21 04:01:18 +0000

    preserving php in ckeditor
    git-svn-id: https://placeto.svn.sourceforge.net/svnroot/placeto/trunk@146 10ae5500-964b-49ae-bf62-554531ab5dc9

--- admin/include/editor.js
+++ admin/include/editor.js
@@ -1,3 +1,21 @@
+/**
+*	Placeto CMS - Admin
+*		The Placeto CMS Administration application.
+*
+*	Copyright (C) 2009-2010 BlaherTech
+*
+*	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*	This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*	You should have received a copy of the GNU General Public License along with this program, as license.txt.  If not, see <http://www.gnu.org/licenses/>.
+*
+*	Author: Benjamin Jay Young
+*		http://www.blahertech.org/projects/placeto/
+*
+*	//////////////////////////////////////////////////
+*
+*	Handles the intitalization of the two textarea editors and the switching events.
+*/
+
 function placeto_editor(menuobj)
 {
 	//get the objects
@@ -17,7 +35,6 @@ function placeto_editor(menuobj)
 	{
 		CKEDITOR.replace('cnt');
 		selch='cke';
-		
 	}
 	else if (chosenoption.value=="source")
 	{
@@ -54,7 +71,8 @@ function placeto_editor(menuobj)
 }
 
 //turn on CKEditor
-var editor=CKEDITOR.replace('cnt');
+CKEDITOR.config.protectedSource.push(/<\?[\s\S]*?\?>/g);
+CKEDITOR.replace('cnt');
 var selch='cke';
 
 //set for EditArea for later

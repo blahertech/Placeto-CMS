623690a03fc90393d9d01723f2c8a6408eaac6df
Benjamin Jay Young <byoung@blahertech.com>
2012-02-11 11:34:40 -0500

    adding some files to placeto library

---
+++ Placeto/placeto/library/placeto/module.class.php
@@ -0,0 +1,6 @@
+<?php
+	class placeto_Module
+	{
+		public function load($strModule){} // load a parent or child module, include file with apc
+	}
+?>
---
+++ Placeto/placeto/library/placeto/template.class.php
@@ -0,0 +1,7 @@
+<?php
+	class placeto_Template
+	{
+		public function set($strTemplate, $strParent=false){} //sets the template
+		public function load() // loads child or parent file
+	}
+?>

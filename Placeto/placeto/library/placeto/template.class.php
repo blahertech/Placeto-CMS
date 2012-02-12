910106272372787fb3df2e373ca3f9ecb063ff9f
Benjamin Jay Young <byoung@blahertech.com>
2012-02-12 13:46:17 -0500

    template update on placeto lib

--- Placeto/placeto/library/placeto/template.class.php
+++ Placeto/placeto/library/placeto/template.class.php
@@ -2,6 +2,6 @@
 	class placeto_Template
 	{
 		public function set($strTemplate, $strParent=false){} //sets the template
-		public function load() // loads child or parent file
+		public function load(){} // loads child or parent file
 	}
 ?>
